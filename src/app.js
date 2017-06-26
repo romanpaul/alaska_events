e237e8d541605666024f2952a4332f23024d0bdd
Roman Paul <smalldarkfilms@gmail.com>
2017-06-26 19:23:33 -0400

    form is POSTing

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
-'use strict'
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 
-const express = require('express');
-const router = require('./api');
+app.use(express.static('public'));
 
-const app = express();
-
-app.use('/', express.static('public'));
-
-app.use('/api', router);
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(3000, function() {
     console.log("The mighty eye is watching on port 3000");
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/events', (req, res) => {
+  console.log(req.body)
 })
