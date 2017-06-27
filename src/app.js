9d2a5d336e0a6576458591b334fd8d10ff272fdb
Roman Paul <smalldarkfilms@gmail.com>
2017-06-27 18:51:56 -0400

    Saving to mLab successfully

--- src/app.js
+++ src/app.js
@@ -1,19 +1,33 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://admin:admin@ds141242.mlab.com:41242/alaska-events', (err, database) => {
+ if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
 
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.listen(3000, function() {
-    console.log("The mighty eye is watching on port 3000");
-})
+//app.listen(3000, function() {
+//    console.log("The mighty eye is watching on port 3000");
+//}
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/events', (req, res) => {
-  console.log(req.body)
+db.collection('events').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
 })
