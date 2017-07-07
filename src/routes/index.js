315174b9c5d69a340998c3b0afce6c18bed19f82
Roman Paul <smalldarkfilms@gmail.com>
2017-07-06 22:04:06 -0400

    Changed POST typo

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res){
  });
 
 router.post('/events', (req, res) => {
-    Event.create(req.body).then(function(ninja){
+    Event.create(req.body).then(function(events){
         res.send(event);
     });
 });
